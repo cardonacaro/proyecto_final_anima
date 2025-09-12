db0114def8d7155ad5a948597bb7ebb1fed4f1d9
GiancarloFornara <gfornara5@gmail.com>
2025-09-12 09:48:21 -0300

    USUARIOS - COMPRADORES
    Creacion de NOMBRE, APELLIDO, TEL, DIRECCION, MAIL Y CONTRASEÑA.
    Falta agregar las ordenes de los usuarios pero esto va a quedar para mas adelante cuando quede pronto orders ya que hay que juntar una con otra y que users reciba la info de ordes para cada usuario.

--- models/User.js
+++ models/User.js
@@ -11,9 +11,27 @@ class User extends Model {
         },
         firstname: {
           type: DataTypes.STRING,
+          allowNull: false,
         },
         lastname: {
           type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        address: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        phone: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
         },
       },
       {
@@ -24,5 +42,11 @@ class User extends Model {
     return User;
   }
 }
+
+//Asociacio que va a tener las ordenes de los usuarios
+// Con respecto a los usuarios
+// Es decir, traemos las ordenes a los usuarios y a cada
+// Usuario le damos su orden
+// JAJAJA No se si me explico pero esto va a quedar pendiente
 
 module.exports = User;
