88486103cbe37894ba6fee346624aedae07f174a
cardonacaro <caro.ssj28@gmail.com>
2025-10-01 20:08:58 -0300

    Autenticación en admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { expressjwt: expressJwt } = require("express-jwt");
 
-router.get("/", adminController.index);
+const autenticacion = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+});
+
+router.get("/", autenticacion, adminController.index);
 router.post("/", adminController.store);
-router.get("/:id", adminController.show);
-router.patch("/:id", adminController.update);
-router.delete("/:id", adminController.destroy);
+router.get("/:id", autenticacion, adminController.show);
+router.patch("/:id", autenticacion, adminController.update);
+router.delete("/:id", autenticacion, adminController.destroy);
 
 router.post("/login", adminController.login);
 
